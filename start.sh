2f7fbb38a3fb99491e2ada2f3088652f8e4da94e
Krzysiek Bielicki <krzysiek@skyman.pl>
2024-12-26 11:18:33 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 #uncomment following line to use in python virtual environment
-#source bin/activate
+source ../venv/bin/activate
 
-./LYWSD03MMC.py --passive --watchdogtimer 5 #--mqttconfigfile mqtt.conf
+./LYWSD03MMC.py --passive --watchdogtimer 5 --mqttconfigfile mqtt.conf -df sensors.ini
 
 
