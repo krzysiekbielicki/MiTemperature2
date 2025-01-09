a128eae5675540f781ba0162e282dee8d7cb67c9
Krzysztof Bielicki <krzysztof.bielicki@allegro.com>
2025-01-09 01:26:10 +0100

    removed unused code

--- LYWSD03MMC.py
+++ LYWSD03MMC.py
@@ -15,20 +15,16 @@ This file explains very detailed about the usage and covers everything you need 
 print(readme)
 
 
-from bluepy import btle
 import argparse
 import os
 import re
 from dataclasses import dataclass
-from collections import deque
 import threading
 import time
 import signal
-import traceback
 import math
 import logging
 import json
-import requests
 import ssl
 
 
@@ -50,8 +46,6 @@ class Measurement:
 		else:
 			return False
 
-measurements=deque()
-#globalBatteryLevel=0
 previousMeasurements={}
 previousCallbacks={}
 identicalCounters={}
@@ -138,81 +132,7 @@ def calibrateHumidity2Points(humidity, offset1, offset2, calpoint1, calpoint2):
 
 
 mode="round"
-class MyDelegate(btle.DefaultDelegate):
-	def __init__(self, params):
-		btle.DefaultDelegate.__init__(self)
-		# ... initialise here
-	
-	def handleNotification(self, cHandle, data):
-		global measurements
-		try:
-			measurement = Measurement(0,0,0,0,0,0,0,0)
-			if args.influxdb == 1:
-				measurement.timestamp = int((time.time() // 10) * 10)
-			else:
-				measurement.timestamp = int(time.time())
-			temp=int.from_bytes(data[0:2],byteorder='little',signed=True)/100
-			#print("Temp received: " + str(temp))
-			if args.round:
-				#print("Temperature unrounded: " + str(temp
-				
-				if args.debounce:
-					global mode
-					temp*=10
-					intpart = math.floor(temp)
-					fracpart = round(temp - intpart,1)
-					#print("Fracpart: " + str(fracpart))
-					if fracpart >= 0.7:
-						mode="ceil"
-					elif fracpart <= 0.2: #either 0.8 and 0.3 or 0.7 and 0.2 for best even distribution
-						mode="trunc"
-					#print("Modus: " + mode)
-					if mode=="trunc": #only a few times
-						temp=math.trunc(temp)
-					elif mode=="ceil":
-						temp=math.ceil(temp)
-					else:
-						temp=round(temp,0)
-					temp /=10.
-					#print("Debounced temp: " + str(temp))
-				else:
-					temp=round(temp,1)
-			humidity=int.from_bytes(data[2:3],byteorder='little')
-			logging.debug("Temperature: " + str(temp))
-			logging.debug("Humidity: " + str(humidity))
-			voltage=int.from_bytes(data[3:5],byteorder='little') / 1000.
-			logging.debug("Battery voltage:",voltage,"V")
-			measurement.temperature = temp
-			measurement.humidity = humidity
-			measurement.voltage = voltage
-			measurement.sensorname = args.name
-			#if args.battery:
-				#measurement.battery = globalBatteryLevel
-			batteryLevel = min(int(round((voltage - 2.1),2) * 100), 100) #3.1 or above --> 100% 2.1 --> 0 %
-			measurement.battery = batteryLevel
-			logging.debug("Battery level:",batteryLevel)
-
-			if(args.mqttconfigfile):
-				if measurement.calibratedHumidity == 0:
-					measurement.calibratedHumidity = measurement.humidity
-				jsonString=buildJSONString(measurement)
-				myMQTTPublish(MQTTTopic,jsonString)
-				#MQTTClient.publish(MQTTTopic,jsonString,1)
-
-
-		except Exception as e:
-			logging.exception("Fehler")
-		
 # Initialisation  -------
-
-def connect():
-	#print("Interface: " + str(args.interface))
-	p = btle.Peripheral(adress,iface=args.interface)	
-	val=b'\x01\x00'
-	p.writeCharacteristic(0x0038,val,True) #enable notifications of Temperature, Humidity and Battery voltage
-	p.writeCharacteristic(0x0046,b'\xf4\x01\x00',True)
-	p.withDelegate(MyDelegate("abc"))
-	return p
 
 def buildJSONString(measurement):
 	jsonstr = '{"temperature": ' + str(measurement.temperature) + ', "humidity": ' + str(measurement.humidity) + ', "voltage": ' + str(measurement.voltage) \
@@ -232,36 +152,9 @@ def MQTTOnDisconnect(client, userdata,rc):
 
 # Main loop --------
 parser=argparse.ArgumentParser(allow_abbrev=False,epilog=readme)
-parser.add_argument("--device","-d", help="Set the device MAC-Address in format AA:BB:CC:DD:EE:FF",metavar='AA:BB:CC:DD:EE:FF')
 parser.add_argument("--battery","-b", help="Get estimated battery level, in passive mode: Get battery level from device", metavar='', type=int, nargs='?', const=1)
-parser.add_argument("--count","-c", help="Read/Receive N measurements and then exit script", metavar='N', type=int)
-parser.add_argument("--interface","-i", help="Specifiy the interface number to use, e.g. 1 for hci1", metavar='N', type=int, default=0)
-parser.add_argument("--unreachable-count","-urc", help="Exit after N unsuccessful connection tries", metavar='N', type=int, default=0)
 parser.add_argument("--mqttconfigfile","-mcf", help="specify a configurationfile for MQTT-Broker")
 parser.add_argument("--log","-l", help="specify loglevel")
-
-
-rounding = parser.add_argument_group("Rounding and debouncing")
-rounding.add_argument("--round","-r", help="Round temperature to one decimal place (and in passive mode humidity to whole numbers)",action='store_true')
-rounding.add_argument("--debounce","-deb", help="Enable this option to get more stable temperature values, requires -r option",action='store_true')
-
-offsetgroup = parser.add_argument_group("Offset calibration mode")
-offsetgroup.add_argument("--offset","-o", help="Enter an offset to the reported humidity value",type=int)
-
-complexCalibrationGroup=parser.add_argument_group("2 Point Calibration")
-complexCalibrationGroup.add_argument("--TwoPointCalibration","-2p", help="Use complex calibration mode. All arguments below are required",action='store_true')
-complexCalibrationGroup.add_argument("--calpoint1","-p1", help="Enter the first calibration point",type=int)
-complexCalibrationGroup.add_argument("--offset1","-o1", help="Enter the offset for the first calibration point",type=int)
-complexCalibrationGroup.add_argument("--calpoint2","-p2", help="Enter the second calibration point",type=int)
-complexCalibrationGroup.add_argument("--offset2","-o2", help="Enter the offset for the second calibration point",type=int)
-
-callbackgroup = parser.add_argument_group("Callback related arguments")
-callbackgroup.add_argument("--callback","-call", help="Pass the path to a program/script that will be called on each new measurement")
-callbackgroup.add_argument("--httpcallback","-http", help="Pass the URL to a program/script that will be called on each new measurement")
-callbackgroup.add_argument("--name","-n", help="Give this sensor a name reported to the callback script")
-callbackgroup.add_argument("--skipidentical","-skip", help="N consecutive identical measurements won't be reported to callbackfunction",metavar='N', type=int, default=0)
-callbackgroup.add_argument("--callback-interval","-int", help="Only invoke callbackfunction every N seconds, e.g. 600 = 10 minutes",type=int, default=0)
-callbackgroup.add_argument("--influxdb","-infl", help="Optimize for writing data to influxdb,1 timestamp optimization, 2 integer optimization",metavar='N', type=int, default=0)
 
 passivegroup = parser.add_argument_group("Passive mode related arguments")
 passivegroup.add_argument("--passive","-p","--atc","-a", help="Read the data of devices based on BLE advertisements, use --battery to get battery level additionaly in percent",action='store_true')
@@ -270,11 +163,10 @@ passivegroup.add_argument("--devicelistfile","-df",help="Specify a device list f
 passivegroup.add_argument("--onlydevicelist","-odl", help="Only read devices which are in the device list file",action='store_true')
 passivegroup.add_argument("--rssi","-rs", help="Report RSSI via callback",action='store_true')
 
-
 args=parser.parse_args()
 
 if args.log:
-	numeric_level = getattr(logging, args.log.upper(), None)
+	numeric_level = int(args.log)
 	if not isinstance(numeric_level, int):
  	   raise ValueError('Invalid log level: %s' % args.log)
 	logging.basicConfig(level=numeric_level)
@@ -344,99 +236,9 @@ if not args.passive:
 	parser.print_help()
 	os._exit(1)
 
-if not args.name:
-	args.name = args.device
-
 signal.signal(signal.SIGINT, signal_handler)	
 
-if args.device: 
-
-	p=btle.Peripheral()
-	cnt=0
-
-	connected=False
-	#logging.basicConfig(level=logging.DEBUG)
-	logging.basicConfig(level=logging.ERROR)
-	logging.debug("Debug: Starting script...")
-	pid=os.getpid()	
-	bluepypid=None
-	unconnectedTime=None
-	connectionLostCounter=0
-
-	watchdogThread = threading.Thread(target=watchDog_Thread)
-	watchdogThread.start()
-	logging.debug("watchdogThread started")
-
-	while True:
-		try:
-			if not connected:
-				#Bluepy sometimes hangs and makes it even impossible to connect with gatttool as long it is running
-				#on every new connection a new bluepy-helper is called
-				#we now make sure that the old one is really terminated. Even if it hangs a simple kill signal was sufficient to terminate it
-				# if bluepypid is not None:
-					# os.system("kill " + bluepypid)
-					# print("Killed possibly remaining bluepy-helper")
-				# else:
-					# print("bluepy-helper couldn't be determined, killing not allowed")
-						
-				logging.debug("Trying to connect to " + adress)
-				p=connect()
-				# logging.debug("Own PID: "  + str(pid))
-				# pstree=os.popen("pstree -p " + str(pid)).read() #we want to kill only bluepy from our own process tree, because other python scripts have there own bluepy-helper process
-				# logging.debug("PSTree: " + pstree)
-				# try:
-					# bluepypid=re.findall(r'bluepy-helper\((.*)\)',pstree)[0] #Store the bluepypid, to kill it later
-				# except IndexError: #Should not happen since we're now connected
-					# logging.debug("Couldn't find pid of bluepy-helper")				
-				connected=True
-				unconnectedTime=None
-				
-			# if args.battery:
-					# if(cnt % args.battery == 0):
-						# print("Warning the battery option is deprecated, Aqara device always reports 99 % battery")
-						# batt=p.readCharacteristic(0x001b)
-						# batt=int.from_bytes(batt,byteorder="little")
-						# print("Battery-Level: " + str(batt))
-						# globalBatteryLevel = batt
-				
-				
-			if p.waitForNotifications(2000):
-				# handleNotification() was called
-				
-				cnt += 1
-				if args.count is not None and cnt >= args.count:
-					logging.info(str(args.count) + " measurements collected. Exiting in a moment.")
-					p.disconnect()
-					time.sleep(5)
-					#It seems that sometimes bluepy-helper remains and thus prevents a reconnection, so we try killing our own bluepy-helper
-					pstree=os.popen("pstree -p " + str(pid)).read() #we want to kill only bluepy from our own process tree, because other python scripts have there own bluepy-helper process
-					bluepypid=0
-					try:
-						bluepypid=re.findall(r'bluepy-helper\((.*)\)',pstree)[0] #Store the bluepypid, to kill it later
-					except IndexError: #Should normally occur because we're disconnected
-						logging.debug("Couldn't find pid of bluepy-helper")
-					if bluepypid != 0:
-						os.system("kill " + bluepypid)
-						logging.debug("Killed bluepy with pid: " + str(bluepypid))
-					os._exit(0)
-				continue
-		except Exception as e:
-			logging.exception("Connection lost")
-			connectionLostCounter +=1
-			if connected is True: #First connection abort after connected
-				unconnectedTime=int(time.time())
-				connected=False
-			if args.unreachable_count != 0 and connectionLostCounter >= args.unreachable_count:
-				logging.exception("Maximum numbers of unsuccessful connections reached, exiting")
-				os._exit(0)
-			time.sleep(1)
-			logging.debug(e)
-			logging.debug(traceback.format_exc())		
-			
-		logging.debug("Waiting...")
-		# Perhaps do something else here
-
-elif args.passive:
+if args.passive:
 	logging.info("""Script started in passive mode
 	------------------------------
 	In this mode all devices within reach are read out, unless a devicelistfile and --onlydevicelist is specified.
@@ -449,7 +251,6 @@ elif args.passive:
 
 	import sys
 	import bluetooth._bluetooth as bluez
-	import cryptoFunctions
 
 	from bluetooth_utils import (toggle_device,
 								enable_le_scan, parse_le_advertising_events,
@@ -480,11 +281,7 @@ elif args.passive:
 					#print(sensorsnew[sensor]["key"])
 		sensors = sensorsnew
 
-	if args.onlydevicelist and not args.devicelistfile:
-		logging.error("Error: --onlydevicelist requires --devicelistfile <devicelistfile>")
-		os._exit(1)
-
-	dev_id = args.interface  # the bluetooth device is hci0
+	dev_id = 0 # the bluetooth device is hci0
 	toggle_device(dev_id, True)
 	
 	try:
@@ -531,42 +328,6 @@ elif args.passive:
 						batteryVoltage = int(strippedData_str[20:24], 16) / 1000
 						batteryPercent = int(strippedData_str[18:20], 16)
 
-					elif len(strippedData_str) == 30: #Custom format
-						logging.debug("BLE packet - Custom: %s %02x %s %d", mac, adv_type, data_str, rssi)
-						advCounter[macStr] = advNumber
-						temperature = int.from_bytes(bytearray.fromhex(strippedData_str[12:16]),byteorder='little',signed=True) / 100.
-						humidity = int.from_bytes(bytearray.fromhex(strippedData_str[16:20]),byteorder='little',signed=False) / 100.
-						batteryVoltage = int.from_bytes(bytearray.fromhex(strippedData_str[20:24]),byteorder='little',signed=False) / 1000.
-						batteryPercent =  int.from_bytes(bytearray.fromhex(strippedData_str[24:26]),byteorder='little',signed=False)
-
-					elif len(strippedData_str) == 22 or len(strippedData_str) == 16: #encrypted: length 22/11 Bytes on custom format, 16/8 Bytes on ATC1441 Format
-						if macStr in advCounter:
-							lastData = advCounter[macStr]
-						else:
-							lastData = None
-
-						if lastData == None or lastData != strippedData_str:
-							logging.debug("BLE packet - Encrypted: %s %02x %s %d, length: %d", mac, adv_type, data_str, rssi, len(strippedData_str)/2)
-							advCounter[macStr] = strippedData_str
-							if mac in sensors and "key" in sensors[mac]:
-								bindkey = bytes.fromhex(sensors[mac]["key"])
-								macReversed=""
-								for x in range(-1,-len(macStr),-2):
-									macReversed += macStr[x-1] + macStr[x]
-								macReversed = bytes.fromhex(macReversed.lower())
-								#print("New encrypted format, MAC:" , macStr, "Reversed: ", macReversed)
-								lengthHex=data_str[offset-8:offset-6]
-								#lengthHex="0b"
-								ret = cryptoFunctions.decrypt_aes_ccm(bindkey,macReversed,bytes.fromhex(lengthHex + "161a18" + strippedData_str))
-								if ret == None: #Error decrypting
-									return
-								#temperature, humidity, batteryPercent = cryptoFunctions.decrypt_aes_ccm(bindkey,macReversed,bytes.fromhex(lengthHex + "161a18" + strippedData_str))
-								temperature, humidity, batteryPercent = ret
-							else:
-								logging.warning("Warning: No key provided for sensor:"+ mac)
-								return
-						else:
-							return #repeated packet
 					else: #no fitting packet
 						return
 
@@ -580,74 +341,6 @@ elif args.passive:
 				measurement.rssi = rssi
 				return measurement
 
-		def decode_data_lywsdcgq(mac, adv_type, data_str, rssi, measurement):
-			preeamble = "5020aa01"
-			packetStart = data_str.find(preeamble)
-			if (packetStart == -1):
-				return
-			offset = packetStart + len(preeamble)
-			strippedData_str = data_str[offset:offset+28]
-			strippedData_str = data_str[offset:]
-			macStr = mac.replace(":","").upper()
-			dataIdentifier = data_str[(offset+14):(offset+16)].upper()
-
-			if(dataIdentifier == "0D") and not args.onlydevicelist or (dataIdentifier == "0D" and mac in sensors) and len(strippedData_str) == 28:
-				logging.debug("BLE packet - lywsdcgq 0D: %s %02x %s %d", mac, adv_type, data_str, rssi)
-				temperature = int.from_bytes(bytearray.fromhex(strippedData_str[20:24]),byteorder='little',signed=True) / 10.
-				humidity = int.from_bytes(bytearray.fromhex(strippedData_str[24:28]),byteorder='little',signed=True) / 10.
-
-				measurement.humidity = humidity
-				measurement.temperature = temperature
-				measurement.rssi = rssi
-				return measurement
-
-			elif(dataIdentifier == "06") and not args.onlydevicelist or (dataIdentifier == "06" and mac in sensors) and len(strippedData_str) == 24:
-				logging.debug("BLE packet - lywsdcgq 06: %s %02x %s %d", mac, adv_type, data_str, rssi)
-				humidity = int.from_bytes(bytearray.fromhex(strippedData_str[20:24]),byteorder='little',signed=True) / 10.
-
-				measurement.humidity = humidity
-				measurement.rssi = rssi
-				return measurement
-
-			elif(dataIdentifier == "04") and not args.onlydevicelist or (dataIdentifier == "04" and mac in sensors) and len(strippedData_str) == 24:
-				logging.debug("BLE packet - lywsdcgq 04: %s %02x %s %d", mac, adv_type, data_str, rssi)
-				temperature = int.from_bytes(bytearray.fromhex(strippedData_str[20:24]),byteorder='little',signed=True) / 10.
-
-				measurement.temperature = temperature
-				measurement.rssi = rssi
-				return measurement
-
-			elif(dataIdentifier == "0A") and not args.onlydevicelist or (dataIdentifier == "0A" and mac in sensors) and len(strippedData_str) == 22:
-				logging.debug("BLE packet - lywsdcgq 0A: %s %02x %s %d" % (mac, adv_type, data_str, rssi))
-				batteryPercent = int.from_bytes(bytearray.fromhex(strippedData_str[20:22]),byteorder='little',signed=False)
-
-				measurement.battery = batteryPercent
-				measurement.rssi = rssi
-				return measurement
-
-		# Tested with Qingping CGG1 and CGDK2
-		def decode_data_qingping(mac, adv_type, data_str, rssi, measurement):
-			preeamble = "cdfd88"
-			packetStart = data_str.find(preeamble)
-			if (packetStart == -1):
-				return
-			offset = packetStart + len(preeamble)
-			strippedData_str = data_str[offset:offset+32]
-			macStr = mac.replace(":","").upper()
-			dataIdentifier = data_str[(offset-2):offset].upper()
-
-			if(dataIdentifier == "88") and not args.onlydevicelist or (dataIdentifier == "88" and mac in sensors) and len(strippedData_str) == 32:
-				logging.debug("BLE packet - Qingping: %s %02x %s %d" % (mac, adv_type, data_str, rssi))
-				temperature = int.from_bytes(bytearray.fromhex(strippedData_str[18:22]),byteorder='little',signed=True) / 10.
-				humidity = int.from_bytes(bytearray.fromhex(strippedData_str[22:26]),byteorder='little',signed=True) / 10.
-				batteryPercent = int(strippedData_str[30:32], 16)
-
-				measurement.battery = batteryPercent
-				measurement.humidity = humidity
-				measurement.temperature = temperature
-				measurement.rssi = rssi
-				return measurement
-
 		def le_advertise_packet_handler(mac, adv_type, data, rssi):
 			global lastBLEPacketReceived
 			if args.watchdogtimer:
@@ -655,25 +348,11 @@ elif args.passive:
 			lastBLEPacketReceived = time.time()
 			data_str = raw_packet_to_str(data)
 
-			global measurements
 			measurement = Measurement(0,0,0,0,0,0,0,0)
-			measurement = (
-				decode_data_atc(mac, adv_type, data_str, rssi, measurement)
-				or
-				decode_data_lywsdcgq(mac, adv_type, data_str, rssi, measurement)
-				or
-				decode_data_qingping(mac, adv_type, data_str, rssi, measurement)
-			)
+			measurement = decode_data_atc(mac, adv_type, data_str, rssi, measurement)
 
 			if measurement:
-				if args.influxdb == 1:
-					measurement.timestamp = int((time.time() // 10) * 10)
-				else:
-					measurement.timestamp = int(time.time())
-
-				if args.round:
-					measurement.temperature=round(measurement.temperature,1)
-					measurement.humidity=round(measurement.humidity,1)
+				measurement.timestamp = int(time.time())
 
 				if mac in sensors and "sensorname" in sensors[mac]:
 					measurement.sensorname = sensors[mac]["sensorname"]
@@ -700,15 +379,9 @@ elif args.passive:
 				if measurement.calibratedHumidity == 0:
 					measurement.calibratedHumidity = measurement.humidity
 
-				if args.callback or args.httpcallback:
-					measurements.append(measurement)
-
 				if args.mqttconfigfile:
 					jsonString=buildJSONString(measurement)
-					myMQTTPublish(currentMQTTTopic,jsonString)
-					#MQTTClient.publish(currentMQTTTopic,jsonString,1)
 
-				#print("Length:", len(measurements))
 
 		if  args.watchdogtimer:
 			keepingLEScanRunningThread = threading.Thread(target=keepingLEScanRunning)
